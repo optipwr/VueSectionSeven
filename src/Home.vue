4583210966440b5e7ca1d2354771958cec9ee243
Michael Irby <michael.irbstoomuch@gmail.com>
2017-08-15 07:40:15 -0400

    updated for axios

--- src/Home.vue
+++ src/Home.vue
@@ -1,8 +1,12 @@
+import
+
 <template>
     <div>
         <p>Server Status: {{ status }}</p>
         <hr>
         <button @click="changeStatus">Change Status</button>
+        <hr>
+        <button @click="dataRequest">Click For Data</button>
     </div>
 </template>
 
@@ -16,6 +20,9 @@
         methods: {
             changeStatus() {
                 this.status = 'Normal';
+            },
+            dataRequest() {
+
             }
         }
     }
